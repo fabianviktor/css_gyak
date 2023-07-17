3831f26da89ea28ac4eab4febee582d74702f262
fabianviktor <fabian.viktor.kornye@gmail.com>
2023-07-17 22:41:12 +0200

    Első commit

---
+++ Time/move.java
@@ -0,0 +1,10 @@
+interface TimerCharProps {
+    char: string;
+}
+const TimerChar:
+React.FC<TimerCharProps> = (props: TimerCharProps) => {
+    const ref:
+    React.MutableRefObject<HTMLDivElement> = 
+    React.useRef<HTMLDivElement>
+    (null);
+}
